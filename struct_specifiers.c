63483ea405f526cb8fcd26a3758a33bd8cefe3f8
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-07-25 06:58:53 -0500

    A printf function

--- struct_specifiers.c
+++ struct_specifiers.c
@@ -7,5 +7,9 @@ specifier_t specifiers[] = {
 	{'i', print_int},
 	{'%', print_percent},
 	{'b', print_binary},
+	{'u', print_unsigned},
+	{'o', print_octal},
+	{'x', print_hex_lower},
+	{'X', print_hex_upper},
 	{0, NULL}
 };
